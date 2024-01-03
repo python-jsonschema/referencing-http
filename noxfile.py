26df399b7e55a8552da17057739cd624bdd8bfeb
Julian Berman <Julian@GrayVines.com>
2024-01-03 15:38:46 -0500

    Set up the package scaffolding.

---
+++ noxfile.py
@@ -0,0 +1,179 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+import os
+
+import nox
+
+ROOT = Path(__file__).parent
+PYPROJECT = ROOT / "pyproject.toml"
+DOCS = ROOT / "docs"
+
+PACKAGE = ROOT / "referencing_http"
+
+REQUIREMENTS = dict(
+    docs=DOCS / "requirements.txt",
+    tests=ROOT / "test-requirements.txt",
+)
+REQUIREMENTS_IN = {
+    path.parent / f"{path.stem}.in" for path in REQUIREMENTS.values()
+}
+
+
+SUPPORTED = ["pypy3.10", "3.10", "3.11", "3.12"]
+LATEST = SUPPORTED[-1]
+
+nox.options.sessions = []
+
+
+def session(default=True, python=LATEST, **kwargs):  # noqa: D103
+    def _session(fn):
+        if default:
+            nox.options.sessions.append(kwargs.get("name", fn.__name__))
+        return nox.session(python=python, **kwargs)(fn)
+
+    return _session
+
+
+@session(python=SUPPORTED)
+def tests(session):
+    """
+    Run the test suite.
+    """
+    session.install("-r", REQUIREMENTS["tests"])
+
+    if session.posargs and session.posargs[0] == "coverage":
+        if len(session.posargs) > 1 and session.posargs[1] == "github":
+            github = Path(os.environ["GITHUB_STEP_SUMMARY"])
+        else:
+            github = None
+
+        session.install("coverage[toml]")
+        session.run("coverage", "run", "-m", "pytest", PACKAGE)
+        if github is None:
+            session.run("coverage", "report")
+        else:
+            with github.open("a") as summary:
+                summary.write("### Coverage\n\n")
+                summary.flush()  # without a flush, output seems out of order.
+                session.run(
+                    "coverage",
+                    "report",
+                    "--format=markdown",
+                    stdout=summary,
+                )
+    else:
+        session.run("python", "-m", "pytest", *session.posargs, PACKAGE)
+
+
+@session(python=SUPPORTED)
+def audit(session):
+    """
+    Audit Python dependencies for vulnerabilities.
+    """
+    session.install("pip-audit", ROOT)
+    session.run("python", "-m", "pip_audit")
+
+
+@session(tags=["build"])
+def build(session):
+    """
+    Build a distribution suitable for PyPI and check its validity.
+    """
+    session.install("build", "twine")
+    with TemporaryDirectory() as tmpdir:
+        session.run("python", "-m", "build", ROOT, "--outdir", tmpdir)
+        session.run("twine", "check", "--strict", tmpdir + "/*")
+
+
+@session()
+def secrets(session):
+    """
+    Check for accidentally included secrets.
+    """
+    session.install("detect-secrets")
+    session.run("detect-secrets", "scan", ROOT)
+
+
+@session(tags=["style"])
+def style(session):
+    """
+    Check for coding style.
+    """
+    session.install("ruff")
+    session.run("ruff", "check", ROOT)
+
+
+@session()
+def typing(session):
+    """
+    Statically check typing annotations.
+    """
+    session.install("pyright", ROOT)
+    session.run("pyright", PACKAGE)
+
+
+@session(tags=["docs"])
+@nox.parametrize(
+    "builder",
+    [
+        nox.param(name, id=name)
+        for name in [
+            "dirhtml",
+            "doctest",
+            "linkcheck",
+            "man",
+            "spelling",
+        ]
+    ],
+)
+def docs(session, builder):
+    """
+    Build the documentation using various Sphinx builders.
+    """
+    session.install("-r", DOCS / "requirements.txt")
+    with TemporaryDirectory() as tmpdir_str:
+        tmpdir = Path(tmpdir_str)
+        argv = ["-n", "-T", "-W"]
+        if builder != "spelling":
+            argv += ["-q"]
+        posargs = session.posargs or [tmpdir / builder]
+        session.run(
+            "python",
+            "-m",
+            "sphinx",
+            "-b",
+            builder,
+            DOCS,
+            *argv,
+            *posargs,
+        )
+
+
+@session(tags=["docs", "style"], name="docs(style)")
+def docs_style(session):
+    """
+    Check the documentation source style.
+    """
+    session.install(
+        "doc8",
+        "pygments",
+        "pygments-github-lexers",
+    )
+    session.run("python", "-m", "doc8", "--config", PYPROJECT, DOCS)
+
+
+@session(default=False)
+def requirements(session):
+    """
+    Update requirements files.
+    """
+    session.install("pip-tools")
+    for each in REQUIREMENTS_IN:
+        session.run(
+            "pip-compile",
+            "--resolver",
+            "backtracking",
+            "--strip-extras",
+            "-U",
+            each.relative_to(ROOT),
+        )
