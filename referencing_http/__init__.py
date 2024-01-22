b187122657512f57127004f2c51ff6dc88177fdb
Julian Berman <Julian@GrayVines.com>
2024-01-22 16:00:06 -0500

    Add the scaffolding for the API structure.

--- referencing_http/__init__.py
+++ referencing_http/__init__.py
@@ -1,3 +1,8 @@
 """
-Fill me in!
+Serve your JSON registries over HTTP, for humans and machines alike!
 """
+from importlib.resources import files
+
+import yaml
+
+SPEC = yaml.safe_load(files(__package__).joinpath("api.yaml").read_text())
